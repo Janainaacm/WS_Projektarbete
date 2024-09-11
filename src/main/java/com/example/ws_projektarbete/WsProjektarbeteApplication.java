2a3e6af61f384e0287d23bcafeeffddcdb7c0f5e
Janainaacm <janaina.maneiro@gmail.com>
2024-09-11 15:27:45 +0200

    first commit

---
+++ src/main/java/com/example/ws_projektarbete/WsProjektarbeteApplication.java
@@ -0,0 +1,13 @@
+package com.example.ws_projektarbete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsProjektarbeteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsProjektarbeteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ws_projektarbete/WsProjektarbeteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ws_projektarbete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsProjektarbeteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
